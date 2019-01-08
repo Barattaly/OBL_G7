8ee7250d0b4dc39e8323fd197ecb82aa2c711b98
user <user@user-TOSH>
2019-01-08 15:02:30 +0200

    update before merge

--- src/gui/LoginController.java
+++ src/gui/LoginController.java
@@ -8,19 +8,31 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
+
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+
 import javafx.stage.Stage;
 
 
-public class LoginController {
+public class LoginController implements Initializable{
+	
+
+	   
+	 
+	 
+	 @Override
+		public void initialize(URL arg0, ResourceBundle arg1) 
+	 	{
+		 
+			
+			
+		}
+	 
 	
 
 
@@ -52,6 +64,8 @@ public class LoginController {
 		SeondStage2.setScene(scene2);		
 		SeondStage2.show();
   }
+
+	
 
   
     
