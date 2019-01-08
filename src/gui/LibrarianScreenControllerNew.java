d9f7ccf7721682e638642f26b9913f1c1691b1fa
user <user@user-TOSH>
2019-01-08 10:24:53 +0200

    New librarian screen (with menu)

--- src/gui/LibrarianScreenControllerNew.java
+++ src/gui/LibrarianScreenControllerNew.java
@@ -10,20 +10,91 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 
 
 
 public class LibrarianScreenControllerNew implements Initializable 
 {
+	 @FXML
+	 private Pane pane_home , pane_createNewSubscriberCard , pane_searchBook , pane_searchSubscriberCard ;
+
+	 @FXML
+	 private ImageView btn_home , btn_createNewSubscriberCard , btn_searchBook , btn_searchSubscriberCard;
+
+
+	    @FXML
+	    void btn_homeDisplay(MouseEvent event) 
+	    {
+	    	pane_home.setVisible(true);
+	    	pane_createNewSubscriberCard.setVisible(false);
+	    	pane_searchBook.setVisible(false);
+	    	pane_searchSubscriberCard.setVisible(false);
+	    	btn_home.setOpacity(0.5);
+	    	btn_createNewSubscriberCard.setOpacity(1);
+	    	btn_searchBook.setOpacity(1);
+	    	btn_searchSubscriberCard.setOpacity(1);
+	    	
+	    }
+	    
+	    @FXML
+	    void btn_createNewSubscriberCardDisplay(MouseEvent event) 
+	    {
+	    	pane_home.setVisible(false);
+	    	pane_createNewSubscriberCard.setVisible(true);
+	    	pane_searchBook.setVisible(false);
+	    	pane_searchSubscriberCard.setVisible(false);
+	    	btn_home.setOpacity(1);
+	    	btn_createNewSubscriberCard.setOpacity(0.5);
+	    	btn_searchBook.setOpacity(1);
+	    	btn_searchSubscriberCard.setOpacity(1);
+	    }
+
+	 @FXML
+	 void btn_searchBookDisplay(MouseEvent event) 
+	 {
+	    	pane_home.setVisible(false);
+	    	pane_createNewSubscriberCard.setVisible(false);
+	    	pane_searchBook.setVisible(true);
+	    	pane_searchSubscriberCard.setVisible(false);
+	    	btn_home.setOpacity(1);
+	    	btn_createNewSubscriberCard.setOpacity(1);
+	    	btn_searchBook.setOpacity(0.5);
+	    	btn_searchSubscriberCard.setOpacity(1);
+	}
+
+	@FXML
+    void btn_searchSubscriberCardDisplay(MouseEvent event) 
+    {
+	    	pane_home.setVisible(false);
+	    	pane_createNewSubscriberCard.setVisible(false);
+	    	pane_searchBook.setVisible(false);
+	    	pane_searchSubscriberCard.setVisible(true);
+	    	btn_home.setOpacity(1);
+	    	btn_createNewSubscriberCard.setOpacity(1);
+	    	btn_searchBook.setOpacity(1);
+	    	btn_searchSubscriberCard.setOpacity(0.5);
+	   }
+
+	  
 	
 	
 	public void initialize(URL arg0, ResourceBundle arg1) 
 	   {
+		pane_home.setVisible(true);
+    	pane_createNewSubscriberCard.setVisible(false);
+    	pane_searchBook.setVisible(false);
+    	pane_searchSubscriberCard.setVisible(false);
+    	btn_home.setOpacity(0.5);
+    	btn_createNewSubscriberCard.setOpacity(1);
+    	btn_searchBook.setOpacity(1);
+    	btn_searchSubscriberCard.setOpacity(1);
 		
-				
+		
 			
 	   }
 	
@@ -43,10 +114,26 @@ public class LibrarianScreenControllerNew implements Initializable
       else if (option.get() == ButtonType.CANCEL) {
     	  alert.close();
     	
-      }
+      }	
      
      
-  
+
+    }
+    
+    @FXML
+    void btn_createSubscriberCardDisplay(ActionEvent event) 
+    {
+    	
+    	Alert alert = new Alert(AlertType.INFORMATION);
+    	alert.setTitle("OBL Create new subscriber");
+    	alert.setHeaderText("The subscriber's card was created successfully");
+    	alert.setContentText("Subscriber Number: ______ ");
+    	 Optional<ButtonType> option = alert.showAndWait();
+     if (option.get() == ButtonType.OK) {
+    	 alert.close();
+     }
+        
+     
     	
 
     }
