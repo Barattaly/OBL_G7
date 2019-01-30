c6e63fccf0a27dbe9319647576eb74e1d1684561
Bar <Bar@10.10.4.54>
2019-01-30 22:22:26 +0200

    automatic method reminderBeforeReturnDate is done.

--- OBL_Server/src/srvrDb/AutomaticExecutors.java
+++ OBL_Server/src/srvrDb/AutomaticExecutors.java
@@ -8,10 +8,13 @@ import java.util.GregorianCalendar;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import entities.Book;
+import entities.BooksQueries;
 import entities.BorrowACopyOfBook;
 import entities.BorrowsQueries;
 import entities.OblMessage;
 import entities.OblMessagesQueries;
+import entities.SendEmail;
 import entities.Subscriber;
 import entities.SubscribersQueries;
 
@@ -26,17 +29,17 @@ public class AutomaticExecutors
 		AutomaticExecutors.oblDB = oblDb;
 		ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
 		executor.scheduleAtFixedRate(() -> checkAndUpdateLateReturns(), 0, 15, TimeUnit.MINUTES);
-		/*executor.scheduleAtFixedRate(() -> task2(), 0, 3, TimeUnit.SECONDS);
-		executor.scheduleAtFixedRate(() -> task3(), 0, 1, TimeUnit.MINUTES);*/
+		executor.scheduleAtFixedRate(() -> reminderBeforeReturnDate(), 0, 15, TimeUnit.MINUTES);
+		executor.scheduleAtFixedRate(() -> ordersFulfillmentCheck(), 0, 15, TimeUnit.MINUTES);
 	}
 	
-	/*checkAndUpdateLateReturns
+	/*checkAndUpdateLateReturns:
 	 * This method suppose to run automatically every 24 hours. If subscriber is
 	 * late at return a copy of a book, his status will change to "frozen", and the
 	 * borrow "isReturnedLate" flag will change to "yes".
 	 * If it's the third late of the same subscriber - than message with the subscriber
 	 * details is send to the library manager in order to change subscriber card
-	 * status to "deep freeze" */
+	 * status to "deep freeze" */	
 	static void checkAndUpdateLateReturns()
 	{
 		String query = BorrowsQueries.getCurrentBorrowsTable();
@@ -51,14 +54,14 @@ public class AutomaticExecutors
 				borrowFromBorrowsTable.setSubscriberId(rsCurrentBorrowsTable.getString(2));
 				borrowFromBorrowsTable.setExpectedReturnDate(rsCurrentBorrowsTable.getString(4));
 
-				String expectedBorrowDate = borrowFromBorrowsTable.getExpectedReturnDate();
+				String expectedReturnDate = borrowFromBorrowsTable.getExpectedReturnDate();
 				String returnDate = getCurrentDateAsString();
 
 				Subscriber subscriberToUpdate = new Subscriber(borrowFromBorrowsTable.getSubscriberId());
 				int lateReturnsCount = getSubscriberNumOfLateReturns(subscriberToUpdate);
 				boolean existInArrayList = false;
 				// check if the subscriber is late at return
-				if (LocalDate.parse(returnDate).isAfter(LocalDate.parse(expectedBorrowDate))) 
+				if (LocalDate.parse(returnDate).isAfter(LocalDate.parse(expectedReturnDate))) 
 				{
 					lateReturnsCount++;
 					if (lateReturnsCount >= 3)
@@ -127,6 +130,92 @@ public class AutomaticExecutors
 			oblDB.executeUpdate(query); // add a new message to messages table
 		}
 	}
+	
+	/*reminderBeforeReturnDate:
+	 * This method suppose to run automatically every 24 hours.
+	 * If the current date is one day before return date,
+	 * than a remainder email send to the subscriber who borrowed the book*/	
+	static void reminderBeforeReturnDate()
+	{
+		String query = BorrowsQueries.getCurrentBorrowsTable();
+		ResultSet rsCurrentBorrowsTable = oblDB.executeQuery(query); // get current borrows table
+		BorrowACopyOfBook borrowFromBorrowsTable = null;
+		// in order to send mails to all of the subscribers with the book information
+		ArrayList<BorrowACopyOfBook> subscribersToInform = new ArrayList<BorrowACopyOfBook>();
+
+		ArrayList<Book> books = new ArrayList<Book>();
+		Book book = null;
+		try
+		{
+			while (rsCurrentBorrowsTable.next())
+			{
+				borrowFromBorrowsTable = new BorrowACopyOfBook();
+				borrowFromBorrowsTable.setId(rsCurrentBorrowsTable.getString(1));
+				borrowFromBorrowsTable.setSubscriberId(rsCurrentBorrowsTable.getString(2));
+				borrowFromBorrowsTable.setExpectedReturnDate(rsCurrentBorrowsTable.getString(4));
+				borrowFromBorrowsTable.setBookCatalogNumber(rsCurrentBorrowsTable.getString(7));
+				borrowFromBorrowsTable.setCopyId(rsCurrentBorrowsTable.getString(8));
+				String expectedReturnDate = borrowFromBorrowsTable.getExpectedReturnDate();
+				// check if the subscriber is late at return
+				if (LocalDate.parse(getCurrentDateAsString()).plusDays(1).isEqual(LocalDate.parse(expectedReturnDate))) 
+				{
+					subscribersToInform.add(borrowFromBorrowsTable);
+					book = new Book(borrowFromBorrowsTable.getBookCatalogNumber());
+					books.add(book);
+				}
+			}
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		// add the name of the book to each book
+		for (Book bookToUpdate : books)
+		{
+			query = BooksQueries.getBookName(bookToUpdate);
+			ResultSet rsBookName = oblDB.executeQuery(query); // get book name
+			try
+			{
+				rsBookName.next();
+				bookToUpdate.setName(rsBookName.getString(1));
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		int i = 0;
+		// Send email to all of the subscribers one day before return date 
+		for (BorrowACopyOfBook borrow: subscribersToInform)
+		{
+			Subscriber subscriberToInform = new Subscriber(borrow.getSubscriberId());
+
+			String fullName = null;
+			query = SubscribersQueries.getSubscriberFullInformationByID(subscriberToInform.getId());
+			ResultSet rsSubscriberDetails = oblDB.executeQuery(query); // get subscriber details
+			try
+			{
+				rsSubscriberDetails.next();
+				fullName = rsSubscriberDetails.getString(4).substring(0, 1).toUpperCase()
+						+ rsSubscriberDetails.getString(4).substring(1) + " "
+						+ rsSubscriberDetails.getString(5).substring(0, 1).toUpperCase()
+						+ rsSubscriberDetails.getString(5).substring(1);
+				subscriberToInform.setPhoneNumber(rsSubscriberDetails.getString(6));
+				subscriberToInform.setEmail(rsSubscriberDetails.getString(7));
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			String emailSubject = "Reminder: One day before return day";
+			String emailMessage = "Dear " + fullName + ",\n"
+								+ "Return date of the book: \"" + books.get(i).getName() + "\" "
+								+ "that you have borrowed is tomrrow.\n"
+								+ "Please return the book until tomrrow, or excute borrow extension (if extension is possible).";
+			/* send an email to the subscriber */
+			SendEmail email = new SendEmail();
+			email.sendEmail(subscriberToInform.getEmail(), emailSubject, emailMessage);
+			i++;
+		}
+	}
+	
 	
 	private static String getCurrentDateAsString()
 	{
